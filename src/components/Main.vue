fa73fd048dc03b87b1f8ea422f79309e63dd6082
Oleh Mazur <oleh@four-velocity.com>
2022-11-28 03:44:08 +0100

    :tada: Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-toolbar color="primary">
+    <v-toolbar-title> Лабораторна робота №2 </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <!-- <template v-slot:extension>
+      <v-tabs v-model="tab" fixed-tabs color="secondary">
+        <v-tab value="one">Завдання №1</v-tab>
+        <v-tab value="two">Завдання №2</v-tab>
+        <v-tab value="three">Завдання №3</v-tab>
+        <v-tab value="three">Завдання №4</v-tab>
+      </v-tabs>
+    </template> -->
+  </v-toolbar>
+</template>
