f2032afa5acc823db15221465f44a14d216d7df4
Oleh Mazur <oleh@four-velocity.com>
2022-11-28 05:07:42 +0100

    :one: Task one completed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,46 @@
-<template>
+<template class="main">
   <v-toolbar color="primary">
     <v-toolbar-title> Лабораторна робота №2 </v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <!-- <template v-slot:extension>
+    <template v-slot:extension>
       <v-tabs v-model="tab" fixed-tabs color="secondary">
         <v-tab value="one">Завдання №1</v-tab>
-        <v-tab value="two">Завдання №2</v-tab>
+        <!-- <v-tab value="two">Завдання №2</v-tab>
         <v-tab value="three">Завдання №3</v-tab>
-        <v-tab value="three">Завдання №4</v-tab>
+        <v-tab value="three">Завдання №4</v-tab> -->
       </v-tabs>
-    </template> -->
+    </template>
   </v-toolbar>
+
+  <v-window v-model="tab" class="text-center">
+    <v-window-item value="one">
+      <Task1 />
+    </v-window-item>
+  </v-window>
+  
 </template>
+
+<style scoped>
+  .v-window {
+    height: 100%;
+  }
+
+  .v-window-item {
+    height: 100%;
+  }
+</style>
+
+<script>
+import Task1 from '@/components/Task1.vue';
+
+export default {
+  data: () => ({
+    tab: null,
+  }),
+  components: {
+    Task1
+  }
+}
+</script>
---
+++ src/components/Task1.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="t1-main">
+        <div class="inner" id="t1--1">
+            <v-btn variant="tonal">
+                Button
+            </v-btn>
+        </div>
+        <div class="inner" id="t1--2">
+            <v-card variant="outlined">
+                <v-card-text>
+                    Top Left Text
+                </v-card-text>
+            </v-card>
+            <v-card variant="outlined" class="bottom-right">
+                <v-card-text>
+                    Bottom Right Text
+                </v-card-text>
+            </v-card>
+        </div>
+        <div class="inner" id="t1--3">
+            <v-card variant="outlined">
+                <v-card-text>
+                    Top Right Text
+                </v-card-text>
+            </v-card>
+            <v-card variant="outlined" class="bottom-right">
+                <v-card-text>
+                    Bottom Right Text
+                </v-card-text>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #t1-main {
+        height: 100%;
+        width: 100vw;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+        justify-content: stretch;
+    }
+    .inner {
+        flex: 1;
+        display: flex;
+        flex-direction: row;
+        width: 100vw;
+    }
+
+    #t1--1 button{
+        height: 33%;
+        width: 33%;
+    }
+
+    #t1--2 {
+        background-color: darkgray;
+        justify-content: space-between;
+    }
+
+    #t1--2 .v-card{
+        height: 33%;
+        width: 33%;
+    }
+
+    #t1--2 .bottom-right {
+        align-self: flex-end;
+    }
+
+    #t1--3 {
+        flex-direction: column;
+        gap: 34 %
+    }
+
+    #t1--3 .v-card {
+        height: 33%;
+        width: 33%;
+        margin-left: auto;
+        margin-right: 0;
+    }
+
+</style>
